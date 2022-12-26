31fdc35dd5bae1d13d8d95e9dedbe1373849f798
Pedram <beheshtianpedram@gmail.com>
2022-12-26 20:28:50 +0330

    Revolutionary change and recoding

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
 $(document).ready(function () {
-  $(".nav-button").click(function () {
-    $(".nav-button").toggleClass("change");
+    $(".nav-button").click(function () {
+      $(".nav-button").toggleClass("change");
+    });
+    $('.owl-carousel').owlCarousel({
+      loop:true,
+      margin:10,
+      nav:true,
+      items:1,
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:3
+          },
+          1000:{
+              items:5
+          }
+      }
+  })
   });
-});
