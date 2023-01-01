96349425ff156c0bfe53e833deae87ecc671f3fb
Pedram <beheshtianpedram@gmail.com>
2023-01-01 20:18:33 +0330

    Review part is ended

--- js/main.js
+++ js/main.js
@@ -1,22 +1,47 @@
 $(document).ready(function () {
-    $(".nav-button").click(function () {
-      $(".nav-button").toggleClass("change");
-    });
-    $('.owl-carousel').owlCarousel({
+  // NAV BOTTON
+  $(".nav-button").click(function () {
+    $(".nav-button").toggleClass("change");
+  });
+  // PROJECT SLIDER
+  $("#project-slider").owlCarousel({
+    loop: true,
+    margin: 5,
+    nav: false,
+    smartSpeed: 800,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      1140: {
+        items: 2,
+        center: true,
+      },
+    },
+  });
+    // REVIEW SLIDER
+    $('#review-slider').owlCarousel({
       loop:true,
       margin:10,
-      nav:true,
-      items:1,
+      nav:false,
+      dots:true,
+      smartSpeed:800,
+      center: true,
+      autoplay: true,
+      autoplayTimeout: 4000,
       responsive:{
           0:{
               items:1
           },
           600:{
-              items:3
+              items:1
           },
           1000:{
-              items:5
+              items:1
           }
       }
   })
-  });
+});
